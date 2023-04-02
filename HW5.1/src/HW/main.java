b6dd7e163f0d64a5425f76fbdad27c6038167b32
Сержио <sirozha16@mail.ru>
2023-04-02 20:12:12 +0300

    дз №4, задача 2

---
+++ HW5.1/src/HW/main.java
@@ -0,0 +1,11 @@
+package HW;
+
+import task.PhoneBook;
+
+public class main {
+    public static void main(String[] args) {
+        PhoneBook PhoneBook = new PhoneBook();
+        task.PhoneBook.main();
+
+    }
+}
