e848d68f5ac40c6ed299e552a54c0396e500dd83
Сержио <sirozha16@mail.ru>
2023-04-02 19:46:25 +0300

    дз №4, задача 1

---
+++ HW4.1/src/HW/Main.java
@@ -0,0 +1,29 @@
+package HW;
+
+//Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+import java.util.LinkedList;
+import java.util.Random;
+
+import static task.reverseLinkedList.reverseLinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        Random random = new Random();
+
+        // добавление случайных элементов в список
+        for (int i = 0; i < 5; i++) {
+            list.add(random.nextInt(10));
+        }
+
+        System.out.println("Список до переворота: " + list);
+
+        // переворот списка
+        LinkedList<Integer> reversedList = reverseLinkedList(list);
+
+
+        System.out.println("Список после переворота: " + reversedList);
+    }
+}
